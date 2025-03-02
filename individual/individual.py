a7b7a8b58b079cb63034567c4f4b323dcca6f50c
Andrey Russia <andrejvolodin35@gmail.com>
2025-03-02 15:27:08 +0300

    Оформление кода в виде функции

--- individual/individual.py
+++ individual/individual.py
@@ -1,4 +1,8 @@
 import random
 
-random_list = [random.randint(5, 6 * 100) for i in range(16)]
+def generate_random_list(num_elements):
+    random_list = [random.randint(5, 6 * 100) for i in range(num_elements)]
+    return random_list
+ 
+random_list = generate_random_list(16)
 print(random_list)
